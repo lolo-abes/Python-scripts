5b549209a75e71fef89509ac9e746fb9b4c4fb77
lolo-abes <test@ffff.frr>
2024-01-29 20:52:45 +0100

    demo des basic J1

---
+++ Basics/general.py
@@ -0,0 +1,21 @@
+# ECRIRE UN COMMENTAIRE SUR UNE LIGNE, ON UTILISE UN "#"
+# COMME CECI
+
+# ECRIRE UN COMMENTAIRE SUR PLUSIEURS LIGNES """ commentaire ligne 1, commentaire ligne 2, ... """
+"""
+ AUTHEUR DU PROGRAMME : LOLO
+ DATE : 29/01/2024
+ RESUME : Programme qui sert à montrer les bases de Python
+"""
+
+# PLUSIEURS MANIERES POUR DECLARER DES VARIABLES SELON LES TYPES
+## INTEGER (ENTIER)
+maVariableEntiere=4
+## STRING OU CHAR (CHAINE DE CARACTERE)
+monString="ceci est une chaine de caractère entre guillemets"
+monString2='ceci est une chaine de caractère entre apostrophes'
+aidonia="nom de code du chanteur: \nligne 1"
+aidonia2='nom de code du chanteur: \nvoice 1'
+
+print(f"voici ma var aidonia {aidonia} \net maVaribaleEntiere = {maVariableEntiere}")
+print("voici ma variable : ",maVariableEntiere,"on continue",aidonia)
